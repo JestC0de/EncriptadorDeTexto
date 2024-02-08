eefee8a1f8c9cb1ede9695d8224ee2a605fdee4a
JestC0de <hubaldo.a.arce@gmail.com>
2024-02-07 21:43:23 -0600

    quitando comentarios

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 function validarTexto(mensaje) {
-    // Expresión regular para validar si solo contiene letras minúsculas y espacios
     const regex = /^[a-z\s]+$/;
     return regex.test(mensaje);
 }
 
 function mostrarAdvertencia(mostrar) {
-    // Mostrar u ocultar el mensaje de advertencia
     const warningMessage = document.getElementById('warning-message');
     warningMessage.style.display = mostrar ? 'block' : 'none';
 }
@@ -14,11 +12,11 @@ function procesarTexto() {
     let inputText = document.getElementById('input-text').value;
 
     if (!validarTexto(inputText)) {
-        mostrarAdvertencia(true); // Mostrar mensaje de advertencia
+        mostrarAdvertencia(true); 
         return;
     }
 
-    mostrarAdvertencia(false); // Ocultar mensaje de advertencia
+    mostrarAdvertencia(false); 
 
     let outputText = "";
 
@@ -54,22 +52,21 @@ function desencriptarTexto(mensaje) {
     let textoFinal = "";
 
     for (let i = 0; i < texto.length; i++) {
-        // Verificar si encontramos una letra que puede ser desencriptada
         if (texto[i] === "e" && texto.slice(i, i + 5) === "enter") {
             textoFinal += "e";
-            i += 4; // Saltar las letras encriptadas
+            i += 4; 
         } else if (texto[i] === "i" && texto.slice(i, i + 4) === "imes") {
             textoFinal += "i";
-            i += 3; // Saltar las letras encriptadas
+            i += 3; 
         } else if (texto[i] === "a" && texto.slice(i, i + 2) === "ai") {
             textoFinal += "a";
-            i += 1; // Saltar la letra encriptada
+            i += 1; 
         } else if (texto[i] === "o" && texto.slice(i, i + 4) === "ober") {
             textoFinal += "o";
-            i += 3; // Saltar las letras encriptadas
+            i += 3; 
         } else if (texto[i] === "u" && texto.slice(i, i + 4) === "ufat") {
             textoFinal += "u";
-            i += 3; // Saltar las letras encriptadas
+            i += 3; 
         } else {
             textoFinal += texto[i];
         }
